048660238360a65c1d5ca90cca9e6fb60e2c9f87
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-04-26 15:58:07 +0200

    Implement US1 : Add the possibility the make a deposit on my bank account

---
+++ src/main/java/fr/lcdlv/bankAccount/BankAccount.java
@@ -0,0 +1,18 @@
+package fr.lcdlv.bankAccount;
+
+class BankAccount {
+
+    private double amount;
+
+    BankAccount(double initialAmount) {
+        this.amount = initialAmount;
+    }
+
+    double getTotalAmount() {
+        return this.amount;
+    }
+
+    void makeADepositOf(double amount) {
+        this.amount += amount;
+    }
+}
---
+++ src/test/java/fr/lcdlv/bankAccount/BankAccountTest.java
@@ -0,0 +1,29 @@
+package fr.lcdlv.bankAccount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BankAccountTest {
+
+    @Test
+    void envShouldBeSetupAndRunning() {
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    void asAClientIfISaveMoneyMyBankAccountShouldBeCredited() {
+        BankAccount bankAccount = new BankAccount(0.0);
+        bankAccount.makeADepositOf(100.0);
+
+        assertThat(bankAccount.getTotalAmount()).isEqualTo(100.0);
+    }
+
+    @Test
+    void asAClientIfISaveMoneyMyBankAccountShouldBeIncreasedOfThisAmount() {
+        BankAccount bankAccount = new BankAccount(150.0);
+        bankAccount.makeADepositOf(100.0);
+
+        assertThat(bankAccount.getTotalAmount()).isEqualTo(250.0);
+    }
+}
