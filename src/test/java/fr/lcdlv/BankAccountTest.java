929e0334b78199e61b7854af9bddff860fe50ec5
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-04-26 15:16:15 +0200

    Initial commit

---
+++ src/test/java/fr/lcdlv/BankAccountTest.java
@@ -0,0 +1,13 @@
+package fr.lcdlv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BankAccountTest {
+
+    @Test
+    void envShouldBeSetupAndRunning() {
+        assertThat(true).isTrue();
+    }
+}
